5d9946da2c59201ae9dc971848e9ddc35c16cb02
donjuanplatinum <donjuan@lecturify.net>
2024-06-09 08:53:43 -0400

    del roma

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,9 +3,9 @@
 (setq org-src-fontify-natively t)
 ;;快速输入
 (global-set-key (kbd "M-o") 'org-insert-structure-template)
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "~/org_note"))
-  )
+;; (use-package org-roam
+;;   :ensure t
+;;   :custom
+;;   (org-roam-directory (file-truename "~/org_note"))
+;;   )
 (provide 'init-org)
