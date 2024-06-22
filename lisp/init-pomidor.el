0ea990564a9d92195125600eed375c07dedcfa9c
donjuanplatinum <donjuan@lecturify.net>
2024-06-21 22:43:01 -0400

    add pomidor

---
+++ lisp/init-pomidor.el
@@ -0,0 +1,11 @@
+(use-package pomidor
+  :bind (("C-c T" . pomidor))
+  :config (setq pomidor-sound-tick nil
+                pomidor-sound-tack nil)
+  :hook (pomidor-mode . (lambda ()
+                          (display-line-numbers-mode -1) ; Emacs 26.1+
+                          (setq left-fringe-width 0 right-fringe-width 0)
+                          (setq left-margin-width 2 right-margin-width 0)
+                          ;; force fringe update
+                          (set-window-buffer nil (current-buffer)))))
+(provide 'init-pomido)
