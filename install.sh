e7c7e690338c3d17ef14ab3d7a94e04027a928b9
唐璜 <113148619+donjuanplatinum@users.noreply.github.com>
2025-05-04 00:28:16 +0800

    fix init and install.sh (#31)

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "${PATH}"
 EMACS_HOME="${USER_HOME}/.emacs.d"
 
-if [ -d "$EMACS_HOME" ]; then
+if [ -e "$EMACS_HOME" ]; then
     echo "$TARGET_DIR 已存在，是否覆盖？(y/n)"
     read -r answer
     if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
@@ -12,12 +12,9 @@ if [ -d "$EMACS_HOME" ]; then
         exit 1
     fi
     # 删除旧目录
-    rm -rf "$TARGET_DIR"
+    /usr/bin/rm -rf "$EMACS_HOME"
 fi
 
 echo "Copying ${PATH} Into ${EMACS_HOME}"
-cp -R "$PATH" "$EMACS_HOME"
-
-
-
-
+/usr/bin/cp -r "$PATH" "$EMACS_HOME"
+/usr/bin/emacs --batch ~/.emacs.d/init.org --eval '(org-babel-tangle)'
